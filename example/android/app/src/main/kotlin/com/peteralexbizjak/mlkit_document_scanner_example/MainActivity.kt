4cb247e535bd82450e678f2e2fd8c378e92447fb
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2024-03-13 13:12:32 +0100

    Extend FlutterFragmentActivity, not the FlutterActivity in the example project

--- example/android/app/src/main/kotlin/com/peteralexbizjak/mlkit_document_scanner_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/peteralexbizjak/mlkit_document_scanner_example/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.peteralexbizjak.mlkit_document_scanner_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
